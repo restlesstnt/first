84565c14687793dfecaa057c77c041e569fc6094
Harrison Ng <88042590+restlesstnt@users.noreply.github.com>
2021-07-27 14:17:22 +0000

    Created a simple program that asks the user for input, and calculates the average + bill average for diners

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # This is a practice on Python.
+import turtle
+
+input1 = int(input("Enter a number: "))
+input2 = int(input("Enter a number: "))
+input3 = int(input("Enter a number: "))
+input4 = int(input("Enter a number: "))
+
+print("This is your average: ") + str((input1 + input2 + input3 + input4) / 4)
+
+total = int(input("What is your total bill?"))
+diners = int(input("How many diners ate this meal?"))
+
+print("This is how much each person should pay: " + str(total / diners))
