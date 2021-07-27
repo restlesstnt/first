e6550d1fd1cbc042f732daae8083c966a95446ae
Harrison Ng <88042590+restlesstnt@users.noreply.github.com>
2021-07-27 14:53:34 +0000

    Made a circle and a star.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # This is a practice on Python.
 import turtle
-
-input1 = int(input("Enter a number: "))
-input2 = int(input("Enter a number: "))
-input3 = int(input("Enter a number: "))
-input4 = int(input("Enter a number: "))
-
-print("This is your average: ") + str((input1 + input2 + input3 + input4) / 4)
-
-total = int(input("What is your total bill?"))
-diners = int(input("How many diners ate this meal?"))
-
-print("This is how much each person should pay: " + str(total / diners))
+import math
+t = turtle.Turtle()
+'''
+# Circle
+for c in ['red', 'green', 'yellow', 'blue', 'purple']:
+  for i in range(0,7):
+    t.color(c)
+    t.forward(9)
+    t.left(45)
+    t.forward(9)
+    t.right(35)
+    t.forward(9)
+'''
+# Star
+for c in ['red', 'green', 'yellow', 'blue', 'purple']:
+    t.color(c)
+    t.right(72)
+    t.forward(50)
+    t.left(145)
+    t.forward(50)
